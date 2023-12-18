43802d7ff9b70982bdb04c3094d1405db5bbb385
encodedevv <timothy.onyeacholam@nxghubdigitech.onmicrosoft.com>
2023-12-18 12:33:22 +0100

    error fix on button

--- components/shared/AddTodo.tsx
+++ components/shared/AddTodo.tsx
@@ -8,7 +8,7 @@ const AddTodo = () => {
     <Form action={create} className="w-1/2 m-auto">
       <div className="flex">
         <Input name="input" type="text" placeholder="Add Todo..." />
-        <Button type="submit" text="Add" />
+        <Button actionButton type="submit" text="Add" />
       </div>
     </Form>
   );
